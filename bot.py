1193dae81d53a9a066f626b1f6c797c9c9cfacde
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 23:57:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ from pyrogram import Client, filters
 
 API_ID = 16844842
 API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
-BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe
+BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
+
 
 # Create the Pyrogram client object
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
