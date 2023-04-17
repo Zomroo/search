c8b2519aa9ceb406a2ca573413bb9a3333d57856
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-18 02:13:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TELEGRAPH_TOKEN = "8bbe8974d80380a9bc560ce2c91443ab78bc182a8d04a6be0c6c7bcb6038"
 TELEGRAM_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
-YANDEX_API_KEY = "pdct.1.1.20230417T203954Z.89329fe738fb735b.df3cfd241dbca08dede3ec358ba389728758852f"
+YANDEX_API_KEY = "pdct.1.1.20230417T173414Z.11f0fa2998c61794.8f5c7895468d7cb94d573c07710a39ad856eefc2"
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Send me an image and I'll upload it to telegra.ph for you.")
@@ -36,22 +36,13 @@ def handle_image(update, context):
     search_url = 'https://yandex.com/images/search'
     headers = {'Authorization': 'Api-Key ' + YANDEX_API_KEY}
     params = {'url': telegraph_url, 'rpt': 'imageview'}
-    try:
-        response = requests.get(search_url, headers=headers, params=params)
-        response.raise_for_status()
-        data = response.json()
-    except requests.exceptions.HTTPError as e:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Error: {}".format(str(e)))
-        return
-    except requests.exceptions.JSONDecodeError as e:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Error decoding JSON response from Yandex: {}".format(str(e)))
-        return
+    response = requests.get(search_url, headers=headers, params=params)
+    print(response.text)
+    response.raise_for_status()
 
-    if 'items' not in data or not data['items']:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="No results found for image search.")
-        return
 
-    best_match_url = data['items'][0]['url']
+    # Get the URL of the best match
+    best_match_url = response.json()['items'][0]['url'] if len(response.json()['items']) > 0 else 'No results found.'
 
     # Send the best match URL to the user
     context.bot.send_message(chat_id=update.effective_chat.id, text=best_match_url)
@@ -59,13 +50,11 @@ def handle_image(update, context):
     # Delete the image
     os.remove('image.jpg')
 
-def main():
+
+if __name__ == '__main__':
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler(Filters.photo, handle_image))
     updater.start_polling()
     updater.idle()
-
-if __name__ == '__main__':
-    main()
