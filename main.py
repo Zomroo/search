e7ee2ea66cd7682c11a5533e5ab7fb447b9a5f1e
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-18 00:10:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,40 @@
-import os
-import requests
 from pyrogram import Client, filters
-from pyrogram.types import Message
-
-# Set up your Yandex API key
-YANDEX_API_KEY = "pdct.1.1.20230417T173414Z.11f0fa2998c61794.8f5c7895468d7cb94d573c07710a39ad856eefc2"
+import requests
+import json
+import os
 
 # Set up your Pyrogram API ID, API hash, and bot token
 API_ID = 16844842
 API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
 BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
 
-# Set up the Telegraph access token
-TELEGRAPH_ACCESS_TOKEN = "8bbe8974d80380a9bc560ce2c91443ab78bc182a8d04a6be0c6c7bcb6038"
-
-# Create the Pyrogram client object
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# Define the command handler for the /start command
 @app.on_message(filters.command("start"))
-def start_command_handler(client: Client, message: Message):
-    message.reply_text("Hi! Send me an image and I'll try to find the waifu or character's name for you.")
+async def start_command(client, message):
+    await message.reply_text("Hi! Send me an image and I'll find the topmost similar result.")
 
-# Define the message handler for images
 @app.on_message(filters.photo)
-def image_handler(client: Client, message: Message):
-    # Download the image from Telegram
-    file_path = client.download_media(message.photo.file_id)
+async def download_and_upload_image(client, message):
+    photo = message.photo[-1] # Get the largest photo size
+    file_path = await client.download_media(photo, file_name="image.jpg") # Download the image to a temporary file
+    with open(file_path, "rb") as f:
+        files = {"file": f}
+        response = requests.post("https://telegra.ph/upload", files=files) # Upload the image to telegra.ph
+        result = response.json()[0]
+        telegraph_url = "https://telegra.ph" + result["src"]
+    os.remove(file_path) # Delete the temporary file
+    yandex_url = "https://www.yandex.com/images/search?rpt=imageview&url=" + telegraph_url # Generate the Yandex search URL
+    response = requests.get(yandex_url) # Search for the image on Yandex
+    html = response.text
+    start_index = html.find('img_url=')
+    if start_index == -1:
+        await message.reply_text("Sorry, I couldn't find any results.")
+        return
+    start_index += len('img_url=') + 1
+    end_index = html.find('&amp;', start_index)
+    if end_index == -1:
+        end_index = len(html)
+    result_url = html[start_index:end_index]
+    result_url = result_url.encode().decode('unicode-escape')
+    await message.reply_text(result_url) # Reply with the topmost result URL
 
-    # Upload the image to Telegraph
-    telegraph_url = upload_to_telegraph(file_path)
-
-    # Search Yandex for the image using the Telegraph link
-    yandex_result = search_yandex(telegraph_url)
-
-    # Reply with the Yandex result
-    message.reply_text(f"Result: {yandex_result}")
-
-def upload_to_telegraph(file_path: str) -> str:
-    # Set up the request URL and parameters
-    url = "https://telegra.ph/upload"
-    files = {"file": ("image.jpg", open(file_path, "rb"))}
-    headers = {"Authorization": f"Bearer {TELEGRAPH_ACCESS_TOKEN}"}
-
-    # Send the request to Telegraph and parse the response
-    response = requests.post(url, files=files, headers=headers)
-    data = response.json()
-
-    # Check if the "src" key is present
-    if "src" in data:
-        return data["src"]
-
-    # Return an error message if no src was found
-    return "Sorry, I couldn't upload the image to Telegraph."
-
-def search_yandex(telegraph_url: str) -> str:
-    # Set up the request URL and parameters
-    url = "https://yandex.com/images/search"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"rpt": "imageview", "format": "json", "request": "{\"blocks\":[{\"block\":\"b-page_type_search-by-image__link\"}],\"params\":{\"url\":\"" + telegraph_url + "\",\"rpt\":\"imageview\"}}"}
-    headers["Authorization"] = "Api-Key " + YANDEX_API_KEY
-
-    # Send the request to Yandex and parse the response
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-
-    # Check if the "blocks" key is present
-    if "blocks" in data:
-        # Get the name from the first block
-
-        name = data["blocks"][0]["params"].get("name")
-        if name:
-            return name
-
-    # Return an error message if no name was found
-    return "Sorry, I couldn't find the name of the character in this image."
-
-
-# Start the Pyrogram client
-app.run()
+app.run() # Run the bot
