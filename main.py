44836468ad117c4306ef80ac35305d6c85e4904a
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 23:13:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 import requests
 
 # Set up your Yandex API key
-YANDEX_API_KEY = os.environ.get("pdct.1.1.20230417T173414Z.11f0fa2998c61794.8f5c7895468d7cb94d573c07710a39ad856eefc2")
+YANDEX_API_KEY = "pdct.1.1.20230417T173414Z.11f0fa2998c61794.8f5c7895468d7cb94d573c07710a39ad856eefc2"
 
 # Set up your Pyrogram API ID, API hash, and bot token
 API_ID = 16844842
