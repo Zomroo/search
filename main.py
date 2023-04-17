661effee4f62793144e5265c9be0c7493adc3140
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 23:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import requests
 YANDEX_API_KEY = os.environ.get("pdct.1.1.20230417T173414Z.11f0fa2998c61794.8f5c7895468d7cb94d573c07710a39ad856eefc2")
 
 # Set up your Pyrogram API ID, API hash, and bot token
-API_ID = os.environ.get("16844842")
-API_HASH = os.environ.get("f6b0ceec5535804be7a56ac71d08a5d4")
-BOT_TOKEN = os.environ.get("5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4")
+API_ID = 16844842
+API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
+BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
 
 # Create the Pyrogram client object
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
